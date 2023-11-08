909544e7492c33b6d3eafeeaa3d0bf23d33e7dab
lixuanye1994 <1121795911@qq.com>
2023-11-08 17:04:19 +0800

    删除多余文件 更换成vue打包文件

---
+++ dist/assets/department-365f22b5.js
@@ -0,0 +1,1 @@
+import{G as e}from"./index-dc0fea34.js";const r={};function t(c,n){return" 机构管理 "}const o=e(r,[["render",t]]);export{o as default};
---
+++ dist/assets/userlevel-346f6686.js
@@ -0,0 +1,1 @@
+import{G as e}from"./index-dc0fea34.js";const r={};function c(t,n){return" 权限管理 "}const _=e(r,[["render",c]]);export{_ as default};
